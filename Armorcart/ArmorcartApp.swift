d088e139fd2fa97a8f5e0d6b4ca0f14ff8698a06
Jigar Dave <jdavenz8@gmail.com>
2021-05-12 16:55:15 +0530

    Initial Commit

---
+++ Armorcart/ArmorcartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArmorcartApp.swift
+//  Armorcart
+//
+//  Created by jigar dave on 12/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ArmorcartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
